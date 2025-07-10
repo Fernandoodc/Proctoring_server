27b43cff7a5a7db7f96d78e311711f6684b55331
Fernandoodc <55900225+Fernandoodc@users.noreply.github.com>
2025-07-10 20:14:36 -0300

    Inicializador de DB

--- inicializar.py
+++ inicializar.py
@@ -1,27 +1,52 @@
 from src.database import models as dbModels
 from src.database.db_mysql import engine, get_connection
 from src.database.models import TipoUsuarios, Usuarios
+from werkzeug.security import generate_password_hash
+from config import settings
 
 dbModels.Base.metadata.create_all(bind=engine)
 connection = get_connection()
 
-TipoDeUsuarios = [
-    {
-        "id":1,
-        "descripcion": "Administrador"
-    },
-    {
-        "id":2,
-        "descripcion":"Docente"
-    },
-    {
-        "id":3,
-        "descripcion":"Estudiante"
-    }
-]
-for tipo in TipoDeUsuarios:
-    connection = get_connection()
-    tipoUser = TipoUsuarios(id_tipo=tipo["id"], descripcion=tipo["descripcion"])
-    connection.add(tipoUser)
+try:
+    #crear tipos de usuarios
+    print("Inicializando tipos de usuarios...")
+    TipoDeUsuarios = [
+        {
+            "id":1,
+            "descripcion": "Administrador"
+        },
+        {
+            "id":2,
+            "descripcion":"Docente"
+        },
+        {
+            "id":3,
+            "descripcion":"Estudiante"
+        }
+    ]
+    for tipo in TipoDeUsuarios:
+        connection = get_connection()
+        tipoUser = TipoUsuarios(id_tipo=tipo["id"], descripcion=tipo["descripcion"])
+        connection.add(tipoUser)
+        connection.commit()
+    print("Tipos de usuarios creados exitosamente.")    
+    #crear usuario administrador
+    print("Inicializando usuario administrador...")
+    usuarioAdmin = Usuarios(
+        username = "admin",
+            password = generate_password_hash("admin", method=settings.HASH),
+            nombre = "Administrador",
+            documento = "12345678",
+            id_tipo = 1,  # Asignar el tipo de usuario administrador
+            activo = True
+        )
+    connection.add(usuarioAdmin)
+    print("Usuario administrador creado exitosamente. \nUsuario: admin \nContraseña: admin")
     connection.commit()
-connection.close()
+    print("Base de datos inicializada correctamente.")
+except Exception as e:
+    print(f"Error al inicializar la base de datos: {e}")
+    connection.rollback()  # Deshacer cambios en caso de error
+finally:
+    # Cerrar la conexión
+    connection.close()
